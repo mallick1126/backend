91d4860551d079813296034798ebb697570210c7
mallick1126 <rahulmallick1126@gmail.com>
2024-01-10 20:47:51 +0530

    setup and connected database

---
+++ src/db/connection.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log(`DB connected successfully! DB host:`, connectionInstance.connection.host);
+    } catch (error) {
+        console.error(`Error connecting to the DB! err:`, error );
+        process.exit(1)
+    }
+}
+
+export default connectDB;
