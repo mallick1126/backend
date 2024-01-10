57e797eac9342ff4ad5a69121069f9dd3be44b65
mallick1126 <rahulmallick1126@gmail.com>
2024-01-11 00:31:58 +0530

    added express configuration

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "snapstream";
+export const limit = "20kb"
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
+import { app } from "./app.js";
+const PORT = process.env.PORT || 8080;
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.error("Server error:", err.message);
+    });
+
+    app.listen(PORT || 8080, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((dbError) => {
+    console.error(`Database connection failed, err: `, dbError);
+  });
