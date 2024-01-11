4d5ae0edfdd5233f29163a0e3bac15190a51b930
mallick1126 <rahulmallick1126@gmail.com>
2024-01-11 16:43:56 +0530

    added hash password functionalities

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -51,5 +53,27 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+/***
+ * now mongoose provides us with some `${hooks (middleware functions)}` that helps us to perform certain action with our data
+ * refer to mongoose middleware documentation to learn more about it.
+ * here we will use *** pre hook *** to hash the password before the user save their detials.
+ */
+
+/*** we'll check if the password field has been modified if its not,
+ * the function returns `next()` to proceed with the saving process.
+ * we'll check this otherwise the password will get hashed everytime user changes anything and saves it.
+ */
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+/*This method is used to compare a given password with the hashed password stored in the user document. */
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
